3bef629d15def727c22071a9e53612f7e01e7f99
Baru <216474718+6aru@users.noreply.github.com>
2026-06-20 18:37:50 +0530

    Update rofi-notifications.sh

--- i3/scripts/rofi-notifications.sh
+++ i3/scripts/rofi-notifications.sh
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
-theme="$HOME/.config/i3/rofi/style-1.rasi"
+theme="$HOME/.config/i3/rofi/notifications.rasi"
 
-option1="󰍡 Notifications"
-option2="󰂛 Toggle DND"
-option3="󰎟 Clear All"
+menu=$(
+{
+echo "󰋚 Show Latest Notification"
+echo "󰨚 Toggle Do Not Disturb"
+echo "󰎟 Clear Notifications"
+echo "────────────────────"
 
-chosen=$(printf "%s\n%s\n%s\n" \
-"$option1" \
-"$option2" \
-"$option3" | rofi \
--dmenu \
--p "Notifications" \
--theme "$theme")
+```
+dunstctl history | jq -r '
+    .data[0][] |
+    .message.data
+' 2>/dev/null
+```
 
-case "$chosen" in
-    "$option1")
-        dunstctl history-pop
-        ;;
-    "$option2")
-        paused=$(dunstctl is-paused)
+} | rofi 
+-dmenu 
+-i 
+-p "Notification Center" 
+-theme "$theme"
+)
 
-        if [ "$paused" = "true" ]; then
-            dunstctl set-paused false
-            notify-send "Notifications Enabled"
-        else
-            dunstctl set-paused true
-            notify-send "Do Not Disturb Enabled"
-        fi
-        ;;
-    "$option3")
-        dunstctl close-all
-        ;;
+case "$menu" in
+"󰂚 Show Latest Notification")
+dunstctl history-pop
+;;
+"󰂛 Toggle Do Not Disturb")
+if [ "$(dunstctl is-paused)" = "true" ]; then
+dunstctl set-paused false
+notify-send "Notifications Enabled"
+else
+dunstctl set-paused true
+notify-send "Do Not Disturb Enabled"
+fi
+;;
+"󰎟 Clear Notifications")
+dunstctl close-all
+;;
 esac
